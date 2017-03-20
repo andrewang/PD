34225322d3d880210e28f6cac245f2958f2dc57b
shpz <jiege_v587@163.com>
2017-03-20 11:35:01 +0800

    虚拟摇杆
    虚拟摇杆

--- assets/scripts/JoyStick.js
+++ assets/scripts/JoyStick.js
@@ -1,64 +1,103 @@
+/**
+ *  虚拟摇杆
+ *  @author shpoz
+ */
+
+// 世界坐标系下底座节点的坐标
+var nodeWorldX = 0;
+var nodeWorldY = 0;
+
+// ..
+var isMove = false;
+
+// 弧度
+var angle = 0;
+
+// 底座圆半径
+var radius = 0;
+
+// 转换至节点坐标系
+// @param pos 世界坐标
+// @return pos 节点坐标
+var convertToNodePos = function(pos){
+
+    if(pos.x > nodeWorldX){
+        pos.x = pos.x - nodeWorldX;
+    }
+    else {
+        pos.x = -(nodeWorldX - pos.x);
+    }
+
+    if(pos.y > nodeWorldY){
+        pos.y = pos.y - nodeWorldY;
+    }
+    else {
+        pos.y = -(nodeWorldY - pos.y);
+    }
+
+    return pos;
+}
+
 cc.Class({
     extends: cc.Component,
 
     properties: {
         player: cc.Node,
         thumb: cc.Node,
+        velocity: 0
     },
 
     // use this for initialization
     onLoad: function () {
         let self = this;
-        this.radius = this.node.width / 2;
+        this.init();
 
         this.node.on(cc.Node.EventType.TOUCH_MOVE, function(event){
-            let pos = self.convertToNodePos(event.getLocation());
-            let p = cc.p(0, 0);
+            let pos = convertToNodePos(event.getLocation());
 
-            if(pos.x > 50 || pos.x < -50){
-                
-                p.x = Math.cos(Math.atan2(pos.y, pos.x)) * self.radius;
-                p.y = Math.sin(Math.atan2(pos.y, pos.x)) * self.radius;
-                self.thumb.setPosition(p);
+            // 发生触点移动事件则让玩家移动
+            isMove = true;
+
+            // 如果触点（杆）越界，根据弧度使用三角函数算出位置
+            // 如果没越界，杆正常移动
+            angle = Math.atan2(pos.y, pos.x);  
+            if(pos.x > radius || pos.x < -radius || pos.y > radius || pos.y < -radius){             
+                self.thumb.setPosition(
+                    Math.cos(angle) * radius, 
+                    Math.sin(angle) * radius
+                );
             }
             else{
                 self.thumb.setPosition(pos);
             }
-            cc.log("y: " + pos.y);
-            cc.log("pos.x: " + pos.x + ", pos.y: " + pos.y);
             
         }, this);
+
+        // 结束触摸后杆还原
+        this.thumb.on(cc.Node.EventType.TOUCH_END, function(event){ 
+            self.thumb.runAction(cc.moveTo(0.1, cc.p(0, 0)));
+            isMove = false;
+        }, this);
+        this.thumb.on(cc.Node.EventType.TOUCH_CANCEL, function(event){ 
+            self.thumb.runAction(cc.moveTo(0.1, cc.p(0, 0)));
+            isMove = false; 
+        }, this);       
     }, 
 
-    convertToNodePos: function(pos){
-
-        if(pos.x > 100){
-            pos.x = pos.x - 100;
-        }
-        else {
-            pos.x = -(100 - pos.x);
-        }
-
-        if(pos.y > 100){
-            pos.y = pos.y - 100;
-        }
-        else {
-            pos.y = -(100 - pos.y);
-        }
-        return pos;
+    // 初始化
+    init: function(){
+        
+        radius = this.node.width / 2;
+        nodeWorldX = this.node.position.x;
+        nodeWorldY = this.node.position.y;   
     },
 
-    calculateY: function(x){
-        let pow = function (param){
-            return param * param;
+    update: function(dt){
+
+        // 根据摇杆移动弧度用三角函数算出移动距离和方向
+        if(isMove){
+            this.player.setPosition(this.player.position.x + Math.cos(angle) * this.velocity * dt, this.player.position.y + Math.sin(angle) * this.velocity * dt);
         }
 
-        return Math.sqrt(pow(this.radius) - pow(x - 0)) + 0;
-    },
-
-
-    // called every frame, uncomment this function to activate update callback
-    // update: function (dt) {
-
-    // },
+    }
 });
